9468b139fbc093ad8ea70a8ee89149a178609df4
Arthur G <agraham@WINDELL-9DU9KRP.localdomain>
2017-11-17 14:34:16 +0000

    changes to header logo and nav menu

--- header.php
+++ header.php
@@ -71,7 +71,8 @@
 </button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-		</div>
+		</div> 
+		<!-- closing div for site-nav -->
 		<?php do_action('hoteli_contact_btn');?>
 	</header><!-- #masthead -->
 
