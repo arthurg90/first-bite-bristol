582526652f981f897b1e44ac56cbe5a0c2d0a11a
Arthur G <agraham@WINDELL-9DU9KRP.localdomain>
2017-11-17 16:44:45 +0000

    add social media icons to footer

--- footer.php
+++ footer.php
@@ -17,8 +17,26 @@
 		<div class="site-info">
 			<?php printf( esc_html__( 'Site created by Arthur Graham ©2017'), 'WordPress' ); ?>
 			<span class="sep">  </span>
-			
+
 		</div><!-- .site-info -->
+
+		<div class="social-media">
+			<a href="[full link to your Twitter]">
+			<img title="Twitter" alt="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter.png" width="35" height="35" />
+			</a>
+			<a href="[full link to your Pinterest]">
+			<img title="Pinterest" alt="Pinterest" src="https://socialmediawidgets.files.wordpress.com/2014/03/13_pinterest.png" width="35" height="35" />
+			</a>
+			<a href="[full link to your Facebook page]">
+			<img title="Facebook" alt="Facebook" src="https://socialmediawidgets.files.wordpress.com/2014/03/02_facebook.png" width="35" height="35" />
+			</a>
+			<a href="[full link to your LinkedIn]">
+			<img title="LinkedIn" alt="LinkedIn" src="https://socialmediawidgets.files.wordpress.com/2014/03/07_linkedin.png" width="35" height="35" />
+			</a>
+			<a href="[full link to your Instagram]">
+			<img title="Instagram" alt="RSS" src="https://socialmediawidgets.files.wordpress.com/2014/03/10_instagram.png" width="35" height="35" />
+			</a>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
