64a6eef61c9570e893cd35847f944454b3147fcd
Arthur G <agraham@WINDELL-9DU9KRP.localdomain>
2018-01-10 11:02:44 +0000

    changes to footer and style

--- footer.php
+++ footer.php
@@ -24,9 +24,6 @@
 			<a href="[full link to your Twitter]">
 			<img title="Twitter" alt="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter.png" width="35" height="35" />
 			</a>
-			<a href="[full link to your Pinterest]">
-			<img title="Pinterest" alt="Pinterest" src="https://socialmediawidgets.files.wordpress.com/2014/03/13_pinterest.png" width="35" height="35" />
-			</a>
 			<a href="[full link to your Facebook page]">
 			<img title="Facebook" alt="Facebook" src="https://socialmediawidgets.files.wordpress.com/2014/03/02_facebook.png" width="35" height="35" />
 			</a>
